4669cbc8b60118f8ded766b4e80861c98111c982
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2022-06-16 12:45:28 -0400

    Started new js code to select the screen size and add the class flex-row

--- js/settings-scripts.js
+++ js/settings-scripts.js
@@ -2,3 +2,10 @@
 // Need to remove class for .flex-column and replace it with .flex-itmes-start at the 400px breakpoint, so that we can have the .nav-pills div side by side with .tab-content or .tab-panes div's.
 
 // We will have to think about the sizing of the tab-nav and content.
+
+const settingsContainer = document.getElementById ('settings-container');
+
+if (window.screen.width() > window.screen.width('767')) {
+    settingsContainer.classList.remove('flex-column');
+    settingsContainer.classList.add('flex-row');
+}
