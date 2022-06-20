bcd267ea22174deb34e5e09dbcd182596e338b23
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2022-06-20 09:23:27 -0400

    Created function for updating classes for the #settings-container

--- js/settings-scripts.js
+++ js/settings-scripts.js
@@ -4,8 +4,20 @@
 // We will have to think about the sizing of the tab-nav and content.
 
 const settingsContainer = document.getElementById ('settings-container');
+const screenHeight = screen.height;
+const screenWidth = screen.width;
+const x = window.matchMedia("(min-width: 768px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 
-if (window.screen.width() > window.screen.width('767')) {
+function myFunction(x) {
+if (x.matches) { // If media query matches
     settingsContainer.classList.remove('flex-column');
     settingsContainer.classList.add('flex-row');
+} else {
+    settingsContainer.classList.remove('flex-row');
+    settingsContainer.classList.add('flex-column');
 }
+}
+
+console.log(myFunction);
