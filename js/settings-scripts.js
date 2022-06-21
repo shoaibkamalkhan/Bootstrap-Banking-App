ca65390445fc3e47e7f2d50cd1678217aeff24e9
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2022-06-21 10:35:36 -0400

    Finished all edits

--- js/settings-scripts.js
+++ js/settings-scripts.js
@@ -1,7 +1,5 @@
 
-// Need to remove class for .flex-column and replace it with .flex-itmes-start at the 400px breakpoint, so that we can have the .nav-pills div side by side with .tab-content or .tab-panes div's.
-
-// We will have to think about the sizing of the tab-nav and content.
+/* Variables */
 
 const settingsContainer = document.getElementById ('settings-container');
 const screenHeight = screen.height;
@@ -10,14 +8,16 @@ const x = window.matchMedia("(min-width: 768px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
 
+/* Functions */
+
 function myFunction(x) {
-if (x.matches) { // If media query matches
-    settingsContainer.classList.remove('flex-column');
-    settingsContainer.classList.add('flex-row');
-} else {
-    settingsContainer.classList.remove('flex-row');
-    settingsContainer.classList.add('flex-column');
-}
+    if (x.matches) { // If media query matches
+        settingsContainer.classList.remove('flex-column');
+        settingsContainer.classList.add('flex-row');
+    } else {
+        settingsContainer.classList.remove('flex-row');
+        settingsContainer.classList.add('flex-column');
+    }
 }
 
 console.log(myFunction);
