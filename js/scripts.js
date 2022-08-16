cddb343bcbeb3961aa93dc57a70632699ca2054c
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2022-08-16 13:06:00 -0400

    Started to implement POST code for the sign in form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
 /* API Code */
 
-fetch('https://27979f46-0c53-4efb-9ef0-608da66d0ff9.mock.pstmn.io/userSessions', { method: 'POST'})
-    .then(results => results.json())
-    .then(console.log)
+const signInForm = document.getElementById('sign-in-form');
+
+signInForm.addEventListener('submit', handleFormSubmit);
+
+async function handleFormSubmit(event) {
+
+/* This allows us to submit the form ourselves and not by browser default */
+    event.preventDefault();
+/* This gets the element which the event handler was attached to. */
+    const form = event.currentTarget;
+/* This takes the API URL from the form's `action` attribute. */
+    const url = form.action;
+}
+
+console.log(signInForm);
+
+
+// fetch('https://27979f46-0c53-4efb-9ef0-608da66d0ff9.mock.pstmn.io/userSessions', { method: 'POST'})
+//     .then(results => results.json())
+//     .then(console.log)
