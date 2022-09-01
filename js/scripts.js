066a3fef5e583dc4317ea04ca67f0b87d8cee2f9
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2022-09-01 12:34:17 -0400

    Updated API Code

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,63 @@ signInForm.addEventListener('submit', handleFormSubmit);
 
 async function handleFormSubmit(event) {
 
-/* This allows us to submit the form ourselves and not by browser default */
+/* This method allows us to submit the form ourselves and not by the browser default */
     event.preventDefault();
-/* This gets the element which the event handler was attached to. */
+/* This variable gets the element which the event handler is attached to. */
     const form = event.currentTarget;
-/* This takes the API URL from the form's `action` attribute. */
+/* This gets the API URL from the form's `action` attribute. */
     const url = form.action;
+
+    try {
+        /* This takes all the fields in the form and makes their values
+		 * available through a `FormData` instance. */
+        const formData = new FormData(form);
+        /* We'll define the `postFormDataAsJson()` function in the next step. */
+        const responseData = await postFormDataAsJson({ url, formData });
+        console.log({ responseData });
+        } catch (error) {
+		console.error(error);
+	}
 }
+
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+	const formDataJsonString = JSON.stringify(plainFormData);
+
+	const fetchOptions = {
+		/**
+		 * The default method for a request with fetch is GET,
+		 * so we must tell it to use the POST HTTP method.
+		 */
+		method: "POST",
+		/**
+		 * These headers will be added to the request and tell
+		 * the API that the request body is JSON and that we can
+		 * accept JSON responses.
+		 */
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		/**
+		 * The body of our POST request is the JSON string that
+		 * we created above.
+		 */
+		body: formDataJsonString,
+	}
+
+    const response = await fetch(url, fetchOptions);
+
+	if (!response.ok) {
+		const errorMessage = await response.text();
+		throw new Error(errorMessage);
+	}
+
+	return response.json();
+
+}
+
+
 
 console.log(signInForm);
 
